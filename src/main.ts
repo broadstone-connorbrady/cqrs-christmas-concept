2b7df62f8be36e3f3688b5720d79a859e8cd7f95
Connor Brady <connorbrady@codox.eu>
2020-12-16 21:51:57 +0000

    WIP Project structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: NestExpressApplication = await NestFactory.create<
+    NestExpressApplication
+    >(AppModule, {
+    logger: console,
+  });
+
+  console.log("TEST");
+
+  await app.startAllMicroservicesAsync();
 }
-bootstrap();
